96370417bb2e08b7a8cfe9357f9085861701531c
isl-90 <isl90@mal.ru>
2023-12-26 13:42:27 +0300

    new

---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,24 @@
+package web.service;
+
+import org.springframework.stereotype.Component;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+@Component
+public class CarServiceImpl implements CarService {
+    private static int CAR_COUNT;
+    private List<Car> carList;
+
+    @Override
+    public List<Car> getCars(int count) {
+        carList = new ArrayList<>();
+        carList.add(new Car(++CAR_COUNT, "AUDI", 4));
+        carList.add(new Car(++CAR_COUNT, "BMW", 3));
+        carList.add(new Car(++CAR_COUNT, "MB", 600));
+        carList.add(new Car(++CAR_COUNT, "Volvo", 90));
+        carList.add(new Car(++CAR_COUNT, "Vaz", 99));
+        return carList.stream().limit(count).collect(Collectors.toList());
+    }
+}
